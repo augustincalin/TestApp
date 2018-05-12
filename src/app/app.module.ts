7fc0973547c8ea6656e20bf176585053c202f04a
Augustin Calin <augustincalin@yahoo.com>
2018-05-13 01:13:02 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TruckLibModule } from 'truck-lib';
 
 import { AppComponent } from './app.component';
+import { CompartmentComponent } from './compartment/compartment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TruckLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/compartment/compartment.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compartment',
+  templateUrl: './compartment.component.html',
+  styleUrls: ['./compartment.component.css']
+})
+export class CompartmentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
